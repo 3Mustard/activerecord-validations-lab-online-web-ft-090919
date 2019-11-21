06958e5a8290c04abc6bc2514bdaf8bb1d54dc3c
Matthew C <m23.cullen@gmail.com>
2019-11-21 04:41:01 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  validates 
+  validates_with  
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
