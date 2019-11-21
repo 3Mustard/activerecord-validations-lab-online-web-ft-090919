5352ec6154e9893b8db8fc65931b48a56d996de0
Matthew C <m23.cullen@gmail.com>
2019-11-21 04:38:52 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,14 +11,5 @@ class Post < ActiveRecord::Base
     /Top [0-9]*/i,
     /Guess/i
   ]
-
-  def not_clickbait
-<<<<<<< HEAD
-    if CLICKBAIT_PATTERNS.none? { |bait| bait.match title }
-=======
-    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
->>>>>>> b009009a15b7b2195342576f0ac17dcced5853e4
-      errors.add(:title, "must be clickbait")
-    end
-  end
+  
 end
