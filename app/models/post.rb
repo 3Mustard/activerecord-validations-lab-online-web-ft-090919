b0efab69a24471d5e077bbd7bc98015a794c2b15
Matthew C <m23.cullen@gmail.com>
2019-11-21 04:40:10 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,10 @@
 class Post < ActiveRecord::Base
   include ActiveModel::Validations
-  
+  validates 
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validate :not_clickbait
   
   CLICKBAIT_PATTERNS = [
     /Won't Believe/i,
