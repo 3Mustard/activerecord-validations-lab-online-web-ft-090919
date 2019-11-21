56a27aaec9b3ab523308b9a000de4db2b6262009
Matthew C <m23.cullen@gmail.com>
2019-11-21 04:45:30 +0000

    Automatically backed up by Learn

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,6 +1,14 @@
 class PostValidator < ActiveModel::Validator
 
   def validate(record)
-  
+    arr = [
+      /Won't Believe/,
+      /Secret/,
+      /Top/,
+      /Guess/
+    ]
+    if arr.none? { |check| check.match title}
+        errors.add(:title, "it must be clickbait")
+    end
   end
 end
